cd1a675b5704b30574c359364889e831f198b7a6
janaravi10 <janaravi962@gmail.com>
2019-05-21 14:42:46 +0530

    Added bodyParser dependency metod for json()

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const db = require("./db_connection/db");
+const bodyParser = require("body-parser");
 // routes
 const userAccess = require("./routes/userAccess");
 // @port for server
 const port = process.env.PORT || 5000;
+// use middleware
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+// routes for user login and signup access
 app.use("/user", userAccess);
-app.get("/hello", function(req, res) {
-  res.send("hello");
-});
+
 app.listen(port, () => console.log("Server started on " + port));
