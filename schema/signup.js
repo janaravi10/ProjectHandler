5dba7bd3e83ebba0a7a175d00bb522d0ddd69442
janaravi10 <janaravi962@gmail.com>
2019-05-21 10:46:37 +0530

    schema for the signup action

---
+++ schema/signup.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// defining schema
+const SignUpSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+// exporting model
+module.exports = mongoose.model("SignUp", SignUpSchema);
