0e0e390c8c8dc544112b2618a8affa11d5c0da49
janaravi10 <janaravi962@gmail.com>
2019-05-21 10:46:14 +0530

    user access routes

--- routes/userAccess.js
+++ routes/userAccess.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", function(req, res) {
-res.send("success");
+// signup model
+const SignUp = require("../schema/signup");
+router.post("/signup", function(req, res) {
+  let email = req.body.email,
+    password = req.body.password;
+    
 });
+
+router.get("/", function(req, res) {
+  res.send("success");
+});
+
 module.exports = router;
