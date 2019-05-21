cdde7659fd71c829275afb1ef65aefb5e61535fb
janaravi10 <janaravi962@gmail.com>
2019-05-21 14:41:47 +0530

    added database name in the connection option

--- db_connection/db.js
+++ db_connection/db.js
@@ -1,18 +1,17 @@
 // database
 const mongoose = require("mongoose");
-const mongoUrl = require("./config/keys").mongoURI;
+const mongoUrl = require("../config/keys").mongoURI;
 console.log(mongoUrl);
 mongoose
   .connect(mongoUrl, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    dbName: "projectHandler",
+    useCreateIndex: true
   })
   .then(() => {
-    /*  # console.log("connected"); */
+    console.log("connected");
   })
   .catch(err => {
     console.log(err);
   });
-var db = mongoose.connection;
-db.on("open", function() {
-  console.log("connected");
-});
+
